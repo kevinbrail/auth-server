fde5670ab27feb2a03efd90a727c0d02b203b7a4
Kevin Brail <kevinabrail@gmail.com>
2019-03-12 13:56:39 -0400

    Added encryption for storage of passwords.  Added token generation for authenticated usersn

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,9 +1,20 @@
+const jwt = require('jwt-simple');
 const User = require('../models/user');
+const config = require('../config');
+
+function tokenForUser(user) {
+    const timestamp = new Date().getTime();
+    return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
 
 exports.signup = function(req, res, next) {
     //see if user email exists
     const email = req.body.email;
     const password = req.body.password;
+
+    if (!email || !password) {
+        return res.status(422).send({ error: 'You must provide email and password'});
+    }
 
     User.findOne({ email: email }, function(err, existingUser) {
         if(err) { return next(err)}
@@ -16,7 +27,7 @@ exports.signup = function(req, res, next) {
         });
         user.save(function(err) {
             if (err) {return next(err); }
-            res.json(user);
+            res.json({ token: tokenForUser(user)});
         });
     })
 
